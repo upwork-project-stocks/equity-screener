6907aa48c47c721038ce65bad4046c3b26034ac4
upwork-project-stocks <132939913+upwork-project-stocks@users.noreply.github.com>
2023-05-10 14:16:59 -0400

    Add files via upload
    added home page for memory allocation testing

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.status(200).end("Equity screener");
+});
+
 module.exports = app;
